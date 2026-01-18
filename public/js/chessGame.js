fe08e1da031c7f6755164a736683138ee80a9afa
Avi shukla <avishuklacode.com>
2026-01-18 16:42:18 +0530

    Whole chessboard logic and rendering it

--- public/js/chessGame.js
+++ public/js/chessGame.js
@@ -1,7 +1,67 @@
 const socket = io();
 
-socket.emit("Apple");
+const chess = new Chess();
 
-socket.on("Apple Pie", function () {
-  console.log("Apple Pie recieved");
-});
+const boardElement = document.querySelector(".chessboard");
+
+let draggedPiece = null;
+let sourceSquare = null;
+let playerRole = null;
+
+const renderBoard = function () {
+  const board = chess.board();
+  boardElement.innerHTML = "";
+  board.forEach((row, rowIndex) => {
+    row.forEach((square, squareIndex) => {
+      const squareElement = document.createElement("div");
+      squareElement.classList.add(
+        "square",
+        (rowIndex + squareIndex) % 2 === 0 ? "light" : "dark"
+      );
+
+      squareElement.dataset.row = rowIndex;
+      squareElement.dataset.col = squareIndex;
+
+      if (square) {
+        const pieceElement = document.createElement("div");
+        pieceElement.classList.add(
+          "piece",
+          square.color === "w" ? "white" : "black"
+        );
+        pieceElement.innerText = "";
+        pieceElement.draggable = playerRole === square.color;
+
+        pieceElement.addEventListener("dragstart", (e) => {
+          if (pieceElement.draggable) {
+            draggedPiece = pieceElement;
+            sourceSquare = { row: rowIndex, col: squareIndex };
+            e.dataTransfer.setData("text/plain", "");
+          }
+        });
+        pieceElement.addEventListener("dragend", (e) => {
+          draggedPiece = null;
+          sourceSquare = null;
+        });
+
+        squareElement.appendChild(pieceElement);
+      }
+      squareElement.addEventListener("dragover", function (e) {
+        e.preventDefault();
+        if (draggedPiece) {
+          targetSource = {
+            row: parseInt(squareElement.dataset.row),
+            col: parseInt(squareElement.dataset.col),
+          };
+          handleMove(sourceSquare, targetSource);
+        }
+      });
+      boardElement.appendChild(squareElement);
+    });
+  });
+};
+
+const handleMove = function () {};
+
+const getPieceUnicode = function () {};
+
+renderBoard();
