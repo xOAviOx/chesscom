de0354e146f5773026c95ebe3a733150b0f47053
Avi shukla <avishuklacode.com>
2026-01-18 15:53:53 +0530

    Player move validation and move logic implementation

--- app.js
+++ app.js
@@ -44,6 +44,26 @@ io.on("connection", function (uniqueSocket) {
       delete players.black;
     }
   });
+
+  uniqueSocket.on("move", (move) => {
+    try {
+      if (chess.turn() === "w" && uniqueSocket.id !== players.white) return;
+      if (chess.turn() === "b" && uniqueSocket.id !== players.black) return;
+
+      const result = chess.move(move);
+      if (result) {
+        currentPlayer = chess.turn();
+        io.emit("move", move);
+        io.emit("boardState", chess.fen());
+      } else {
+        console.log("Invalid move : ", move);
+        uniqueSocket.emit("invalidMove", move);
+      }
+    } catch (err) {
+      console.log(err);
+      uniqueSocket.emit("Invalid move : ", move);
+    }
+  });
 });
 
 server.listen(3000, function () {
